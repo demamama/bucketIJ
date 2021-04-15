cf20daeda0710e38747a172cb33e74ca5399ca9e
demamama <82538255+demamama@users.noreply.github.com>
2021-04-15 21:27:18 +0300

    Add files via upload

---
+++ SpawnPoint.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    [SerializeField] private GameObject _template;
+    [SerializeField] private Transform _spawnPoint;
+    private void Update()
+    {
+        Instantiate(_template, _spawnPoint.position, Quaternion.identity);
+    }
+}
